6513b0bd4988ec698c2951eefe841693d454cd5b
maggiekean <maggie@kean.net>
2012-09-06 09:20:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storit::Application.config.secret_token = 'a350f23b57f683aace7096bd135b48f3c8f2a045d51ca4f3bc36c5236ce59ac1268fd1d59411f2564755a7f80ad5c63e4e463598342cffb12f0f70aa3840a38d'
